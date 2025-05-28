14e8d2f3c455789f0da7642d5e947de28d91cacc
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-05-28 12:27:06 +0100

    Add Docker support and update Eureka server port

--- microservice1/src/main/java/com/errabi/microservice1/controller/HelloController.java
+++ microservice1/src/main/java/com/errabi/microservice1/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.errabi.microservice1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.support.RequestPartServletServerHttpRequest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    public String sayHello() throws UnknownHostException {
+        String port = environment.getProperty("local.server.port");
+        String hostname = InetAddress.getLocalHost().getHostName();
+        System.out.println("Microservice 1 is running on hostname: " + hostname + " and port: " + port);
+        return "Hello, World! from hostname: " + hostname + " and port: " + port;
     }
 }

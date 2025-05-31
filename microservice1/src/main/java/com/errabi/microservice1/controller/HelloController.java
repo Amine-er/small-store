12d1487ee1277294b66e245ed50ddf4da842de47
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-05-31 16:58:32 +0100

    Add API Gateway with Spring Cloud Gateway and Eureka integration; configure routes and Docker support

--- microservice1/src/main/java/com/errabi/microservice1/controller/HelloController.java
+++ microservice1/src/main/java/com/errabi/microservice1/controller/HelloController.java
@@ -3,12 +3,14 @@ package com.errabi.microservice1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @RestController
+@RequestMapping("/microservice1")
 public class HelloController {
 
     @Autowired
--- microservice2/src/main/java/com/errabi/microservice2/controller/WebClientConsumerController.java
+++ microservice2/src/main/java/com/errabi/microservice2/controller/WebClientConsumerController.java
@@ -3,11 +3,13 @@ package com.errabi.microservice2.controller;
 import com.errabi.microservice2.service.WebClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/microservice2")
 public class WebClientConsumerController {
     private final WebClientService webClientService;
 
